d21ea651822d45213aa5d3d5cabf7663f53b2366
lulla-by <thfl3363@hanmail.net>
2024-01-25 17:33:26 +0900

    Lv 0. 옷가게 할인 받기 - Math.floor() :  내림 함수

---
+++ programmers/LV.0/ClothingRetailerDiscount.js
@@ -0,0 +1,12 @@
+// 옷가게 할인 받기
+
+// 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+// 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(price) {
+  let answer = 0;
+  let percent =
+    price >= 500000 ? 20 : price >= 300000 ? 10 : price >= 100000 ? 5 : 0;
+  answer = (price * (100 - percent)) / 100;
+  return Math.floor(answer);
+}
