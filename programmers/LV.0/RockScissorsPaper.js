8c3c013437dc0f9a2f09e890af617971dae0e47a
lulla-by <thfl3363@hanmail.net>
2024-01-31 14:09:25 +0900

    Lv 0. 가위 바위 보

---
+++ programmers/LV.0/RockScissorsPaper.js
@@ -0,0 +1,32 @@
+// 가위 바위 보
+
+// 가위는 2 바위는 0 보는 5로 표현합니다.
+// 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때,
+// rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+
+function solution(rsp) {
+  let answer = '';
+  let arr = rsp.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '2') {
+      answer += '0';
+    } else if (arr[i] === '0') {
+      answer += '5';
+    } else {
+      answer += '2';
+    }
+  }
+  return answer;
+}
+
+// 다른 사람 풀이법
+
+function solution2(rsp) {
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
+}
