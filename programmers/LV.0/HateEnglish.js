50acbf381c7dbb04f72bf997a57dc4706f445cf3
lulla-by <thfl3363@hanmail.net>
2024-02-15 22:36:05 +0900

    Lv 0. 영어가 싫어요 -replaceAll()

---
+++ programmers/LV.0/HateEnglish.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const num= ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for(let i = 0 ;i < num.length;i++){
+        numbers = numbers.replaceAll(num[i],i)
+    }
+    return +numbers;
+}
